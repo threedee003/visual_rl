23f9877468e7934ac253c1ee022536da11192aa0
Tribikram Dhar <74320520+threedee003@users.noreply.github.com>
2025-05-20 18:12:01 +0530

    Update env_inspect.py

--- scripts/env_inspect.py
+++ scripts/env_inspect.py
@@ -34,15 +34,15 @@ print(depth.dtype)
 
 
 '''
-output for the code
+output for the code to understand maniskill observation and action space.
 
-Observation space Dict('agent': Dict('qpos': Box(-inf, inf, (2, 9), float32), 'qvel': Box(-inf, inf, (2, 9), float32)), 'extra': Dict('is_grasped': Box(False, True, (2,), bool), 'tcp_pose': Box(-inf, inf, (2, 7), float32), 'goal_pos': Box(-inf, inf, (2, 3), float32)), 'sensor_param': Dict('base_camera': Dict('extrinsic_cv': Box(-inf, inf, (2, 3, 4), float32), 'cam2world_gl': Box(-inf, inf, (2, 4, 4), float32), 'intrinsic_cv': Box(-inf, inf, (2, 3, 3), float32))), 'sensor_data': Dict('base_camera': Dict('rgb': Box(0, 255, (2, 480, 640, 3), uint8), 'depth': Box(-32768, 32767, (2, 480, 640, 1), int16))))
-Action space Box(-1.0, 1.0, (2, 7), float32)
-torch.Size([2, 480, 640, 3])
+
+Observation space Dict('agent': Dict('qpos': Box(-inf, inf, (1, 9), float32), 'qvel': Box(-inf, inf, (1, 9), float32)), 'extra': Dict('is_grasped': Box(False, True, (1,), bool), 'tcp_pose': Box(-inf, inf, (1, 7), float32), 'goal_pos': Box(-inf, inf, (1, 3), float32)), 'sensor_param': Dict('base_camera': Dict('extrinsic_cv': Box(-inf, inf, (1, 3, 4), float32), 'cam2world_gl': Box(-inf, inf, (1, 4, 4), float32), 'intrinsic_cv': Box(-inf, inf, (1, 3, 3), float32))), 'sensor_data': Dict('base_camera': Dict('depth': Box(-32768, 32767, (1, 480, 640, 1), int16), 'rgb': Box(0, 255, (1, 480, 640, 3), uint8))))
+Action space Box(-1.0, 1.0, (7,), float32)
+torch.Size([1, 480, 640, 3])
 torch.uint8
-torch.Size([2, 480, 640, 1])
+torch.Size([1, 480, 640, 1])
 torch.int16
-
 
 
 
